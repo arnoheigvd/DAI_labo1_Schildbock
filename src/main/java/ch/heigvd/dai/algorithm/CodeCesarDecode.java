c8fd0332408cc6d212daa67cbc4e26e31d84caea
shadowkudo <david.schildbock@heig-vd.ch>
2024-10-14 11:05:55 +0200

    implemented CodeCesar

--- src/main/java/ch/heigvd/dai/algorithm/CodeCesarDecode.java
+++ src/main/java/ch/heigvd/dai/algorithm/CodeCesarDecode.java
@@ -2,12 +2,48 @@ package ch.heigvd.dai.algorithm;
 
 import ch.heigvd.dai.mode.Decrypt;
 
+import java.io.*;
 import java.lang.System;
+import java.nio.charset.StandardCharsets;
 
 public class CodeCesarDecode implements Decrypt {
 
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
     @Override
     public void decode(String filename_input, String filename_output, String key){
-        System.out.println("Test_CodeCesar_Decrypt");
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename_input, StandardCharsets.UTF_8));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(filename_output, StandardCharsets.UTF_8))){
+
+            StringBuilder content = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append(System.lineSeparator());
+            }
+
+            String encryptText = content.toString();
+            encryptText = encryptText.toLowerCase().trim();
+
+            int shiftKey = Integer.parseInt(key);
+
+            String decryptStr = "";
+            for (int i = 0; i < encryptText.length(); i++) {
+                int pos = ALPHABET.indexOf(encryptText.charAt(i));
+
+                int decryptPos = (pos - shiftKey) % 26;
+
+                if (decryptPos < 0){
+                    decryptPos = ALPHABET.length() + decryptPos;
+                }
+                char decryptChar = ALPHABET.charAt(decryptPos);
+
+                decryptStr += decryptChar;
+            }
+            writer.write(decryptStr);
+
+        } catch (IOException e) {
+            System.err.println("Error in decoding CodeCesar -> " + e.getMessage());
+        }
     }
 }
--- src/main/java/ch/heigvd/dai/algorithm/CodeCesarEncode.java
+++ src/main/java/ch/heigvd/dai/algorithm/CodeCesarEncode.java
@@ -2,12 +2,45 @@ package ch.heigvd.dai.algorithm;
 
 import ch.heigvd.dai.mode.Encrypt;
 
+import java.io.*;
 import java.lang.System;
+import java.nio.charset.StandardCharsets;
 
 public class CodeCesarEncode implements Encrypt {
 
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
     @Override
     public void encode(String filename_input, String filename_output, String key){
-        System.out.println("Test_CodeCesar_Encrypt");
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename_input, StandardCharsets.UTF_8));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(filename_output, StandardCharsets.UTF_8))){
+
+            StringBuilder content = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append(System.lineSeparator());
+            }
+
+            String plainText = content.toString();
+            plainText = plainText.toLowerCase().trim();
+
+            int shiftKey = Integer.parseInt(key);
+
+            String encryptText = "";
+            for (int i = 0; i < plainText.length(); i++) {
+                int pos = ALPHABET.indexOf(plainText.charAt(i));
+
+                int encryptPos = (shiftKey + pos) % 26;
+                char encryptChar = ALPHABET.charAt(encryptPos);
+
+                encryptText += encryptChar;
+            }
+
+            writer.write(encryptText);
+
+        } catch (IOException e) {
+            System.err.println("Error in encoding CodeCesar -> " + e.getMessage());
+        }
     }
 }
